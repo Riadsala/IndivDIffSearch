41bb0aa3c605d4da7f592b1d60b21c7f75101471
Alasdair Clarke <a.clarke@essex.ac.uk>
2017-03-01 16:26:14 +0000

    Adding most data from 1-32
    Still some asc files missing though

--- Scripts/lineseg/3_saccadeStrategy.R
+++ Scripts/lineseg/3_saccadeStrategy.R
@@ -34,29 +34,5 @@ plt = plt + scale_x_continuous(name="fixation number", breaks=c(2,4,6,8,10))
 plt = plt + scale_y_continuous(name="proportion of fixations to heterogeneous side", limits=c(0,1))
 ggsave("scratch/strategyBySessionAndPerson.pdf")
 
-metricVal = data.frame(nfix=as.numeric(), r=as.numeric())
-for (nf in 2:12)
-{
-  d = aggregate(propHetro ~ subj, data=filter(aggData, n<=nf), "mean")
-  cordf = merge(d, medianRT)
-  r = cor(cordf$propHetro, cordf$RT)
-  metricVal = rbind(metricVal, data.frame(nfix=nf, r=r))
-}
-
-plt = ggplot(metricVal, aes(x=nfix, y=r^2)) + geom_point()
-plt = plt + scale_x_continuous(name = "including first x fixations", breaks=c(2,4,6,8,10))
-plt = plt + scale_y_continuous(name = expression(R^2))
-plt = plt + theme_bw()
-ggsave("r_by_nfix.pdf", width=4, height=4)
-
-nf = 3
-d = aggregate(propHetro ~ subj, data=filter(aggData, fixNum<=nf), "mean")
-cordf = merge(d, medianRT)
- plt = ggplot(cordf, aes(x=propHetro, y=RT)) + geom_point()
- plt = plt + geom_smooth(method=lm)
- plt = plt + scale_x_continuous("mean prop. of first 3 fixations on hetro. side", limits=c(0,1))
- plt = plt + scale_y_continuous("median reaction time (secs")
- plt = plt + theme_bw()
-ggsave("best_r_scatter.pdf", width=4, height=4)
 
 
