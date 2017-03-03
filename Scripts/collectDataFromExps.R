2c933b7aa6b3c8a8614cadae93d788270291bd94
Alasdair Clarke <a.clarke@essex.ac.uk>
2017-03-03 17:28:50 +0000

    pre-pub data processing

--- Scripts/collectDataFromExps.R
+++ Scripts/collectDataFromExps.R
@@ -4,11 +4,21 @@ library(dplyr)
 
 lineseg = read.csv("lineseg/scratch/lineseg_output.csv")
 adapt   = read.csv("adaptchoice/Data_AdaptChoice_allsubs_Summary.txt", sep="\t")
+forage  = read.csv("foraging/Data_MultiTargForag_allsubs.txt", sep="\t")
 
-names(adapt) = c("observer", "ac_acc", "ac_rt", "ac_propOpt", "ac_switchRate")
-
+names(adapt) = c("observer", "ac_acc", "ac_rt", "ac_propOpt", "ac_switchRate", "ac_meanlogrt", "ac_medianrt")
+names(forage) = c("observer", "fg_meanrt_feature", "fg_medianrt_feature", "fg_meanlogrt_feature", "fg_runnum_feature", "fg_runlength_feature","fg_meanrt_cong", "fg_medianrt_cong", "fg_meanlogrt_cong", "fg_runnum_cong", "fg_runlength_cong")
 
 dat = merge(lineseg, adapt)
+dat = merge(dat, forage)
+
+
+rtdat = select(dat, ls_meanlogrt_hard, ac_meanlogrt, fg_meanlogrt_feature, fg_meanlogrt_cong)
+
+cor(rtdat)
+
+
+str(cor(dat[,-1]))
 
 
 
