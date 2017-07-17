9552a9bc3c3348d082cf5866a985105e99cbd350
Alasdair Clarke <a.clarke@essex.ac.uk>
2017-07-17 16:15:56 +0100

    more lineseg analysis

--- Scripts/lineseg/2_rtAnalysis.R
+++ Scripts/lineseg/2_rtAnalysis.R
@@ -2,51 +2,104 @@ library(tidyverse)
 library(forcats)
 library(ggthemes)
 library(scales)   
+library(binom)
 
-figXn = 6
-figYn = 4
+figXn <- 6
+figYn <- 4
+
 # read in processed data
-trlDat <- readRDS(file="scratch/processedRTandAccData.Rda")
+trlDat <- readRDS(file = "scratch/processedRTandAccData.Rda")
+fixDat <- readRDS(file = "scratch/processedFixationData.Rda")
 
+# -----------------------------------------------------------------------------
+# look at accuracy 
+# -----------------------------------------------------------------------------
 
-# look at accuracy ---------------------------------------------------------------
-accDat  = aggregate(data=trlDat, accuracy ~ observer + session + targSide, FUN="mean")
-
-plt <- ggplot(accDat, aes(x = targSide, y = accuracy, fill = session))
+acc_dat  <- (trlDat %>% 
+	group_by(observer, session, targSide) %>% 
+	summarise(
+		nTrials = length(accuracy),
+		accuracy = mean(accuracy),
+		lower = binom.confint(nTrials*accuracy, nTrials, method = 'exact')$lower,
+		upper = binom.confint(nTrials*accuracy, nTrials, method = 'exact')$upper))
+acc_dat <- select(acc_dat, -nTrials)
+	
+plt <- ggplot(acc_dat, aes(x = targSide, y = accuracy, fill = session))
 plt <- plt + geom_bar(stat = "identity", position = position_dodge()) 
-plt <- plt + scale_x_continuous(name = "target condition")
+plt <- plt + scale_x_discrete(name = "target condition")
 plt <- plt + facet_wrap( ~ observer)
 plt <- plt + theme_hc() + scale_fill_ptol()
 ggsave("scratch/acc_by_session_by_person.pdf", width=2*figXn, height=2*figYn)
+
+acc_dat %>% unite(accuracy, accuracy, lower, upper) -> acc_dat
+acc_dat <- spread(acc_dat, key = session, value = accuracy )
+
+#  Why are there NAs???
+
+acc_dat %>% 
+	separate(a, into = c("a_acc", "a_lower", "a_upper"), sep = "_", convert = TRUE) %>%
+	separate(b, into = c("b_acc", "b_lower", "b_upper"), sep = "_", convert = TRUE) -> acc_dat
+
+r <- with(filter(acc_dat, targSide == "hard"), cor.test(a_acc, b_acc)$estimate)
+r <- round(r, 2)
+
+# compare hard accuracy between session a and b
+plt <- ggplot(filter(acc_dat, targSide == "hard"), 
+	aes(x = a_acc, xmin = a_lower, xmax = a_upper,
+	  y = b_acc, ymin = b_lower, ymax = b_upper))
+plt <- plt + geom_abline(slope=1, linetype=2)
+plt <- plt + geom_errorbar(size=0.25) + geom_errorbarh(size=0.25)
+plt <- plt + geom_point() 
+plt <- plt + geom_smooth(method = "lm", se=FALSE, colour="black")
+plt <- plt + geom_text(
+	label = paste("italic(r) == ",r), x = 0.25, y = 0.8, parse = TRUE)
+plt <- plt + scale_x_continuous(name = "accuracy session a ", limits = c(0, 1))
+plt <- plt + scale_y_continuous(name = "accuracy session b ", limits = c(0, 1))
+plt <- plt + theme_minimal() + scale_color_ptol(name = "target condition")
+ggsave("scratch/acc_correlation.pdf", width=figYn, height=figYn)
+
 
 # remove person 15 as they never found the hard targets
 trlDat <- filter(trlDat, observer != 15)
 fixDat <- filter(fixDat, observer != 15)
 
+acc_dat  <- (trlDat %>% 
+	group_by(observer, session, targSide) %>% 
+	summarise(
+		nTrials = length(accuracy),
+		accuracy = mean(accuracy),
+		lower = binom.confint(nTrials*accuracy, nTrials, method = 'exact')$lower,
+		upper = binom.confint(nTrials*accuracy, nTrials, method = 'exact')$upper))
+acc_dat <- select(acc_dat, -nTrials)
+
 # plot boxplots
-plt <- ggplot(accDat, aes(x = session, y = accuracy, fill = targSide))
+plt <- ggplot(acc_dat, aes(x = session, y = accuracy, fill = targSide))
 plt <- plt + geom_boxplot()
 plt <- plt + theme_hc() + scale_fill_ptol()
 ggsave("scratch/acc_by_session.pdf", width=figXn, height=figYn)
 
 # only take correct trials
 trlDat <- filter(trlDat, accuracy == 1)
+fixDat <- filter(left_join(fixDat, trlDat), accuracy==1)
 
-# look at RT ---------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# look at RT 
+# -----------------------------------------------------------------------------
 
-plt <- ggplot(trlDat, aes(x = rt, fill = targSide)) + geom_density(alpha=0.7)
-plt <- plt + scale_fill_ptol(name = "target position") + theme_minimal()
-plt <- plt + scale_x_continuous(name = "reaction time (ms)", 
+plt <- ggplot(trlDat, aes(x = rt/1000, fill = targSide)) 
+plt <- plt + geom_density(alpha=0.7)
+plt <- plt + scale_fill_ptol(name = "target position") + theme_hc()
+plt <- plt + scale_x_continuous(name = "reaction time (seconds)", 
 	trans = log2_trans(), 
-	breaks = c(250, 1000, 4000, 16000, 64000)) 
+	breaks = c(250, 1000, 4000, 16000, 64000)/1000) 
 plt <- plt + theme(
 	legend.justification = c(1,1), 
 	legend.position = c(1,1),
 	legend.background = element_rect(fill="white"))
 ggsave("scratch/densityRT.pdf", width=figXn, height=figYn)
-ggsave("scratch/densityRT.png", width=figXn, height=figYn)
 
-rtDat = (filter(trlDat) 
+
+rt_dat <- (filter(trlDat) 
 	%>% group_by(observer, session, targSide)
 	%>% summarise(
 		logrt = mean(log(rt,2)),
@@ -55,45 +108,47 @@ rtDat = (filter(trlDat)
 		upper = logrt + 1.96*sderr,
 		lower = logrt - 1.96*sderr))
 
-rtDat = select(rtDat, observer, session, targSide, logrt, lower, upper)
-rtDat %>% unite(logrt, logrt, lower, upper) -> rtDat
-rtDat <- spread(rtDat, key = session, value = logrt )
-rtDat %>% 
+rt_dat <- select(rt_dat, observer, session, targSide, logrt, lower, upper)
+rt_dat %>% unite(logrt, logrt, lower, upper) -> rt_dat
+rt_dat <- spread(rt_dat, key = session, value = logrt )
+rt_dat %>% 
 	separate(a, into = c("a_log2rt", "a_lower", "a_upper"), sep = "_", convert = TRUE) %>%
-	separate(b, into = c("b_log2rt", "b_lower", "b_upper"), sep = "_", convert = TRUE) -> rtDat
+	separate(b, into = c("b_log2rt", "b_lower", "b_upper"), sep = "_", convert = TRUE) -> rt_dat
 
+# correlations between session a and b
+easy_r = round(with(filter(rt_dat, targSide == "easy"), 
+	cor.test(a_log2rt, b_log2rt)$estimate),2)
 
-plt <- ggplot(rtDat, aes(
-	x = a_log2rt, xmin = a_lower, xmax = a_upper,
-	y = b_log2rt, ymin = b_lower, ymax = b_upper,
+hard_r = round(with(filter(rt_dat, targSide == "hard"), 
+	cor.test(a_log2rt, b_log2rt)$estimate),2)
+
+absent_r = round(with(filter(rt_dat, targSide == "absent"), 
+	cor.test(a_log2rt, b_log2rt)$estimate),2)
+
+r_df = tibble(
+	text = c(
+		paste("r = ", easy_r), paste("r = ", hard_r), paste("r = ", absent_r) ),
+	x = c(9.5, 10.55, 13.75), y = c(10.25, 11.5, 11.25), targSide = c("easy", "hard", "absent"))
+
+plt <- ggplot(rt_dat, aes(
+	x = a_log2rt,#, ,
+	y = b_log2rt,#, ,
 	colour=targSide))
-plt <- plt + geom_point() 
-plt <- plt + geom_errorbar(lineWidth=0.5) + geom_errorbarh(lineWidth=0.5)
-plt <- plt + geom_smooth(method="lm", se=FALSE)
-plt <- plt + theme_minimal() + scale_color_ptol()
 plt <- plt + geom_abline(slope=1, linetype=2)
+plt <- plt + geom_point() 
+plt <- plt + geom_errorbar(size=0.25, aes(ymin = b_lower, ymax = b_upper)) + geom_errorbarh(size=0.25, aes(xmin = a_lower, xmax = a_upper))
+plt <- plt + geom_smooth(method="lm", se=FALSE)
+plt <- plt + theme_minimal() + scale_color_ptol(name = "target condition")
 plt <- plt + scale_x_continuous("session 1: log reaction time (ms)")
 plt <- plt + scale_y_continuous("session 2: log reaction time (ms)")
 plt <- plt + theme(
 	legend.justification = c(0,1), 
 	legend.position = c(0,1),
 	legend.background = element_rect(fill="white"))
+plt <- plt + geom_text(data=r_df, aes(x = x, y = y, label = text, colour=targSide), show.legend = FALSE)
 ggsave("scratch/rt_correlation.pdf", width=figYn, height=figYn)
 
 
-
-
-
-
-
-# # now lets look at RTs... 
-# # first we need to filter out incorrect trials
-# rtdat = rtdat[which(rtdat$acc==1),]
-# library(scales)     
-# # rt for target present and absent
-# rtdat  = aggregate(data=rtdat, RT ~ subj +targSide, FUN="median")
-
-# ggsave("plots/RT.jpg",dpi=600, width=figXn, height=figYn)
 
 
 
--- Scripts/lineseg/3_saccadeStrategy.R
+++ Scripts/lineseg/3_saccadeStrategy.R
@@ -1,99 +1,103 @@
-library(dplyr)
-library(ggplot2)
+library(tidyverse)
+library(ggthemes)
+library(forcats)
 library(binom)
 library(lme4)
 library(scales)
 
-cbPalette <- c("#56B4E9", "#E69F00")
+figXn <- 6
+figYn <- 4
  
-fixDat = readRDS(file="scratch/processedFixationData.Rda")
-trlDat = readRDS(file="scratch/processedRTandAccData.Rda")
+fix_dat <- readRDS(file="scratch/processedFixationData.Rda")
+trl_dat = readRDS(file="scratch/processedRTandAccData.Rda")
 
+# only take correct trials
+trl_dat <- filter(trl_dat, accuracy == 1)
+fix_dat <- filter(left_join(fix_dat, trl_dat), accuracy == 1)
+
+# remove person 15 as they never found the hard targets
+trl_dat <- filter(trl_dat, observer != 15)
+fix_dat <- filter(fix_dat, observer != 15)
 
 
 # classify every fixation as homo (left), central, or hetro (right)
-centralWidth = 0.05 # used to be 64 pixels! #change to 1 visual degree
-fixDat$side = 'central'
-fixDat$side[which(fixDat$x <(0-centralWidth/2))] = "homo"
-fixDat$side[which(fixDat$x >(0+centralWidth/2))] = "hetro"
-fixDat$side = as.factor(fixDat$side)
+centralWidth <- 0.05 # used to be 64 pixels! #change to 1 visual degree
+fix_dat$side <- 'central'
+fix_dat$side[which(fix_dat$x <(0-centralWidth/2))] <- "homo"
+fix_dat$side[which(fix_dat$x >(0+centralWidth/2))] <- "hetero"
+fix_dat$side <- as_factor(fix_dat$side)
 
-
-# remove incorrect trials
-fixDat$acc = 1
-incorrectTrls = filter(trlDat, acc!=1)
-for (ii in 1:nrow(incorrectTrls))
-{
-	idx = which(fixDat$observer==incorrectTrls$observer[ii] & fixDat$session==incorrectTrls$session[ii] & fixDat$trial==incorrectTrls$trial[ii])
-	fixDat$acc[idx] = 0
-}
-
-
-trlDat = filter(trlDat, acc==1)
-fixDat = filter(fixDat, acc==1)
-
-aggData = (filter(fixDat, side!="central", n<12, n>1, targSide=="absent") 
+agg_dat = (filter(fix_dat, side!="central", n<12, n>1, targSide=="absent") 
   %>% group_by(observer, session, n) 
     %>% summarise(
     	nTrials=length(trial),
-     propHetro=mean(side=="hetro"), 
-     lowerS = binom.confint(propHetro*nTrials,nTrials, method='wilson')$lower,
-     upperS = binom.confint(propHetro*nTrials,nTrials, method='wilson')$upper))
+      propHetro=mean(side=="hetero"), 
+      lowerS = binom.confint(propHetro*nTrials,nTrials, method='wilson')$lower,
+      upperS = binom.confint(propHetro*nTrials,nTrials, method='wilson')$upper))
 
-
-plt = ggplot(filter(aggData, n<=5), aes(x=n, y=propHetro, ymin=lowerS, ymax=upperS, colour=session))
-plt = plt + geom_point() + geom_path() + geom_errorbar()
-plt = plt + theme_bw() + facet_wrap(~observer, nrow=3)
-plt = plt + scale_x_continuous(name="fixation number", breaks=c(2,4,6,8,10))
-plt = plt + scale_y_continuous(name="proportion of fixations to heterogeneous side", limits=c(0,1))
+plt <- ggplot(filter(agg_dat, n<=5), 
+  aes(x=n, y=propHetro, ymin=lowerS, ymax=upperS, colour=session))
+plt <- plt + geom_point() + geom_path() + geom_errorbar()
+plt <- plt + facet_wrap(~observer, nrow=3)
+plt <- plt + scale_x_continuous(
+  name="fixation number", breaks=c(2,4,6,8,10))
+plt <- plt + scale_y_continuous(
+  name="proportion of fixations to heterogeneous side", limits=c(0,1))
+plt <- plt + theme_bw() + scale_colour_ptol()
 ggsave("scratch/strategyBySessionAndPerson.pdf", width=12, height=5)
 
 
+# --------------------------------------------------------------------------------
+# how well does search strategy predict RT?
+# --------------------------------------------------------------------------------
+(left_join(fix_dat, trl_dat) 
+  %>% filter(n > 1, n <= 3, side != "central")
+  %>% group_by(observer, session, trial, targSide) 
+  %>% summarise(
+    prop_homo = mean(side == "homo"),
+    rt    = unique(rt))
+  %>% group_by(observer, session, targSide) 
+  %>% summarise(
+    prop_homo  = mean(prop_homo),
+    median_rt = median(rt),
+    meanlogrt = mean(log(rt,2)))
+  ) -> dat
 
-###########
+dat2 <- filter(dat, targSide == "absent")
+dat2$median_rt <- filter(dat, targSide == "hard")$median_rt
+
+r_sess_a = with(filter(dat2, session == "a"), cor.test(prop_homo, median_rt))
+r_sess_a = format(round(r_sess_a$estimate, 2), nsmall = 2)
+r_sess_b = with(filter(dat2, session == "b"), cor.test(prop_homo, median_rt))
+r_sess_b = format(round(r_sess_b$estimate, 2), nsmall = 2)
+
+r_df = tibble(
+  text = c(
+    paste("italic(r) == ", r_sess_a), 
+    paste("italic(r) ==", r_sess_b)),
+  x = c(0.90, 0.90), y = c(5000, 2000), session = c("a", "b"))
+
+plt <- ggplot(dat2, aes(x = prop_homo, y = median_rt, colour = session))
+plt <- plt + geom_point() + geom_smooth(method = lm)
+plt <- plt + theme_minimal() + scale_colour_ptol()
+plt <- plt + scale_x_continuous("prop. homogeneous fixations (absent)", limits = c(0, 1))
+plt <- plt + scale_y_continuous("median reaction time for hard trials (secs)")
+plt <- plt + theme(
+  legend.justification = c(0,1), 
+  legend.position = c(0,1),
+  legend.background = element_rect(fill="white"))
+plt <- plt + geom_text(data=r_df, 
+  aes(x = x, y = y, label = text, colour=session), show.legend = FALSE, parse = TRUE)
+ggsave("./scratch/start_compare_median_rt.pdf", width = figYn, height = figYn)
+
+
+# --------------------------------------------------------------------------------
 # output data for cross experiment correlations!
-
 # base statistic for correlation on only first 3 fixations
-aggDataSS = (filter(fixDat, side!="central", n>1, n<=5, targSide=="absent", session=="a") 
-  %>% group_by(observer) 
-    %>% summarise(
-     propHetro_a=mean(side=="hetro")))
+# use session B only
+# --------------------------------------------------------------------------------
 
-aggDataSSb = (filter(fixDat, side!="central", n>1, n<=5, targSide=="absent", session=="b") 
-  %>% group_by(observer) 
-    %>% summarise(
-     propHetro_b=mean(side=="hetro")))
-
-aggDataSS$propHetro_b = aggDataSSb$propHetro_b
-
-
-aggDataRT = (filter(trlDat, targSide=="hard", session=="a") 
-  %>% group_by(observer) 
-    %>% summarise(
-     medianRTa = median(rt),
-     meanLogRTa = mean(log(rt))))
-
-aggDataRTb = (filter(trlDat, targSide=="hard", session=="b") 
-  %>% group_by(observer) 
-    %>% summarise(
-     medianRTb = median(rt),
-     meanLogRTb = mean(log(rt))))
-
-aggDataRT$medianRTb = aggDataRTb$medianRTb
-aggDataRT$meanLogRTb = aggDataRTb$meanLogRTb
-
-
-
-aggData = merge(aggDataSS, aggDataRT)
-
-
-plt = ggplot(aggData, aes(x=propHetro, y=medianRT, colour=observer))
-plt = plt + geom_point() 
-ggsave("scratch/stratVrt.pdf")
-
-names(aggData) = c("observer", "lsA_propHetro_absent", "lsB_propHetro_absent", "lsA_medianRT_hard", "lsA_meanlogrt_hard", "lsB_medianRT_hard", "lsB_meanlogrt_hard")
-
-write.csv(aggData, "scratch/lineseg_output.csv", row.names=F)
+write_csv(filter(dat2, targSide=="b"), "scratch/lineseg_output.csv")
 
 
 
